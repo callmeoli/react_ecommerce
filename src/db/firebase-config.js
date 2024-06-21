0e933b96e5a6e5fad9157ce3192610077cef285c
callme_oli <olikorma@gmail.com>
2024-06-21 07:48:44 +0000

    push

---
+++ src/db/firebase-config.js
@@ -0,0 +1,27 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBcrxbWiNVtJ0IMA0v57gig8uwcSAuX708",
+  authDomain: "awosomepro64.firebaseapp.com",
+  projectId: "awosomepro64",
+  storageBucket: "awosomepro64.appspot.com",
+  messagingSenderId: "735725448424",
+  appId: "1:735725448424:web:5ece3c679f869ab1372d5d",
+  measurementId: "G-MM26S20S27"
+};
+// export const firebaseConfig = {
+//     apiKey: '',
+//     authDomain: '',
+//     databaseURL: '',
+//     projectId: '',
+//     storageBucket: '',
+//     messagingSenderId: '',
+//     appId: '',
+//   };
+// export const firebaseConfig = {
+//     apiKey: "AIzaSyDI4B5EOIkWoIlb1NXSQRZztfyattHE-Z8",
+//     authDomain: "products-2c1d1.firebaseapp.com",
+//     projectId: "products-2c1d1",
+//     storageBucket: "products-2c1d1.appspot.com",
+//     messagingSenderId: "340601356916",
+//     appId: "1:340601356916:web:197c5f46ba457143180721",
+//     measurementId: "G-47PSTSSQPY"
+//   };
--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -1,1 +1,18 @@
-
+export const firebaseConfig = {
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+  };
+// export const firebaseConfig = {
+//     apiKey: "AIzaSyDI4B5EOIkWoIlb1NXSQRZztfyattHE-Z8",
+//     authDomain: "products-2c1d1.firebaseapp.com",
+//     projectId: "products-2c1d1",
+//     storageBucket: "products-2c1d1.appspot.com",
+//     messagingSenderId: "340601356916",
+//     appId: "1:340601356916:web:197c5f46ba457143180721",
+//     measurementId: "G-47PSTSSQPY"
+//   };
